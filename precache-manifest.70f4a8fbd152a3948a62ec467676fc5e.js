cf303015497f2555994a69f0310e6e6fe4a5ae52
Julius Alexander IV <julius@enlitic.com>
2020-09-26 12:15:25 -0500

    Updates

---
+++ precache-manifest.70f4a8fbd152a3948a62ec467676fc5e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3466093c51303757ed04c29538e73f02",
+    "url": "/euchre/index.html"
+  },
+  {
+    "revision": "f220929cc2f76cd0a559",
+    "url": "/euchre/static/css/main.3e49cf53.chunk.css"
+  },
+  {
+    "revision": "cb822c52693c8f60a2b1",
+    "url": "/euchre/static/js/2.f862ba03.chunk.js"
+  },
+  {
+    "revision": "e6191615acbaa88870aef4cd0368e4ec",
+    "url": "/euchre/static/js/2.f862ba03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f220929cc2f76cd0a559",
+    "url": "/euchre/static/js/main.449b8ab7.chunk.js"
+  },
+  {
+    "revision": "53a633bd55b588ee0a9a",
+    "url": "/euchre/static/js/runtime-main.aa5dc6af.js"
+  }
+]);
