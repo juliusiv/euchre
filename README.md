18c3bd73d17665696eb20bc2cd4325d10cfa4dac
Julius Alexander IV <julius@enlitic.com>
2020-11-05 17:54:26 -0600

    Make it more responsive.

--- README.md
+++ README.md
@@ -8,10 +8,11 @@ A tracker for my family's euchre tournament stats and scores.
 - [ ] Calculate Elo scores and other stats
 - [ ] Figure out how to add historic data that only kept track of the top 3 finishers and their scores
 - [ ] Move data source into Google Sheets
-- [ ] Make sure everything is responsive
+- [x] Make sure everything is responsive
 - [ ] Change favicon and logos
-- [ ] Use Typescript
+- [ ] Use Typescript (partially done)
 - [ ] Write tests?
+- [ ] Adding routing
 
 
 ## Deploy
--- src/pages/TournamentPage/TournamentPage.js
+++ src/pages/TournamentPage/TournamentPage.js
@@ -1,16 +1,18 @@
 import React from "react"
 
 import ScoresTable from "./ScoresTable"
-import ProgressionChart from "./ProgressionChart"
+// import ProgressionChart from "./ProgressionChart"
 
 const TournamentPage = ({ data, ...props }) => {
   return (
     <div>
-      <ScoresTable data={data} />
-
-      <div className="pt-8">
-        <ProgressionChart data={data} />
+      <div className="overflow-x-auto">
+        <ScoresTable data={data} />
       </div>
+
+      {/* <div className="pt-8">
+        <ProgressionChart data={data} />
+      </div> */}
     </div>
   )
 }
